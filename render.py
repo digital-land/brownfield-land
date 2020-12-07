927951cddcf8a652e61458df06601dc1e5fbd2d2
Colm Britton <colmjude@gmail.com>
2020-12-07 16:57:15 +0000

    Render index page, grouped by organisation
    Fixed issue with id generation
    
    Handle empty organisation rows

--- render.py
+++ render.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
+import sys
 import csv
+from collections import OrderedDict
 
 from digital_land_frontend.jinja import setup_jinja
 from digital_land_frontend.render import render
@@ -8,29 +10,75 @@ from digital_land_frontend.jinja_filters.organisation_mapper import Organisation
 
 docs = "docs"
 dataset_csv = "data/dataset.csv"
+data_type = "brownfield land"
+
+organisation_mapper = OrganisationMapper()
 
 env = setup_jinja()
-env.globals["urlRoot"] = "/brownfield-land"
+env.globals["urlRoot"] = "/brownfield-land/"
 
 index_template = env.get_template("index.html")
 row_template = env.get_template("row.html")
 
 ids = set()
 
+translations = str.maketrans({"/": "-", " ": "", "(": "", ")": "", "'": ""})
+
 
 def get_id(row, idx):
-    id = row["site"]
-    if row["site"] is None or row["site"] in ids:
+    id = row["site"].translate(translations)
+    if not row["site"] or row["site"] in ids:
         id = f"{row['resource']}:{idx}"
     ids.add(id)
     return id
 
 
-for idx, row in enumerate(csv.DictReader(open(dataset_csv)), start=1):
-    row["id"] = get_id(row, idx)
+def by_organisation(rows):
+    by_organisation = {}
+    by_organisation.setdefault(
+        "no-organisation", {"name": "No organisation", "rows": []}
+    )
+    for row in rows:
+        if row["organisation"]:
+            o = {
+                "name": organisation_mapper.get_by_key(row["organisation"]),
+                "rows": [],
+            }
+            by_organisation.setdefault(row["organisation"], o)
+            by_organisation[row["organisation"]]["rows"].append(row)
+        else:
+            by_organisation["no-organisation"]["rows"].append(row)
 
-    try:
-        render(f"{row['site']}/index.html", row_template, row=row)
-    except Exception as e:
-        print(e)
-        print(row["organisation"], idx)
+    result = OrderedDict(sorted(by_organisation.items(), key=lambda x: x[1]["name"]))
+    result.move_to_end("no-organisation")
+    return result
+
+
+def render_pages():
+    failing = []
+    rows = []
+    for idx, row in enumerate(csv.DictReader(open(dataset_csv)), start=1):
+        row["id"] = get_id(row, idx)
+
+        try:
+            render(
+                f"{row['id']}/index.html", row_template, row=row, data_type=data_type
+            )
+            rows.append(row)
+        except Exception as e:
+            failing.append((row["organisation"], idx, e))
+
+    index = {
+        "count": len(rows),
+        "organisation": by_organisation(rows),
+    }
+
+    render("index.html", index_template, index=index, data_type=data_type)
+    print(failing)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1 and sys.argv[1] == "--local":
+        env.globals["urlRoot"] = "/"
+
+    render_pages()
